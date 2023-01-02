719647b1fe60e88709c7eff0f643283eddd6e807
randyver <randyverdian99@gmail.com>
2023-01-02 14:42:16 +0700

    menambahkan materi geometri

---
+++ materi/geometri/segitiga-dan-kesebangunan/script.js
@@ -0,0 +1,25 @@
+form = document.getElementById("practice-form")
+const jawabanBenar = ["70", "70", "300", "3", "10", "13", "83"];
+
+function cekJawaban(){
+    let score = 0;
+    for(let i = 0; i < jawabanBenar.length; i++){
+        if(form[i].value === jawabanBenar[i]){
+            document.getElementById(`tanda${i}`).innerHTML = "&#10004;";
+            score++;
+        }
+        else{
+            document.getElementById(`tanda${i}`).innerHTML = "&#10006;";
+        }
+    }
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("nilai").innerHTML = `Nilai kamu adalah ${((score/jawabanBenar.length)*100).toFixed(2)}/100`;
+    document.getElementById("nilai").style.backgroundColor = "rgb(235, 62, 62)"
+    document.getElementById("reset-kj").style.display = "flex";
+}
+
+function lihatKj(){
+    for(let i = 0; i < jawabanBenar.length; i++){
+        document.getElementById(`kj${i}`).innerHTML = `Jawaban benar : ${jawabanBenar[i]}`;
+    }
+}
