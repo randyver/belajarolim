e7d83c6c94cc173ee00003d156c400ab85968558
randyver <randyverdian99@gmail.com>
2023-01-09 12:39:43 +0700

    menambahkan try out

---
+++ try-out/try-out-3/script.js
@@ -0,0 +1,47 @@
+form = document.getElementById("soal")
+const jawabanBenar = ["12", "2", "18", "4", "85", "3", "61", "48", "31", "407"];
+let score = 0;
+
+function cekJawaban(){
+    for(let i = 0; i < jawabanBenar.length; i++){
+        if(form[i].value === jawabanBenar[i]){
+            score++;
+        }
+    }
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("answer-submit").innerHTML = "Jawaban Anda telah dikirim.";
+    document.getElementById("reset-nilai").style.display = "flex";
+}
+
+function lihatNilai(){
+    alert(`Nilai kamu adalah ${((score/jawabanBenar.length)*100)}/100`);
+}
+
+function confirmSubmit(){
+  if (confirm("Apakah Anda yakin ingin mengumpulkan jawaban Anda?")) {
+    cekJawaban();
+    clearInterval(interval);
+  }
+}
+
+const timer = document.getElementById("timer");
+let hours = 01;
+let minutes = 00;
+let seconds = 00;
+let interval = setInterval(() => {
+    seconds--;
+    if (seconds < 0) {
+      minutes--;
+      seconds = 59;
+    }
+    if (minutes < 0) {
+      hours--;
+      minutes = 59;
+    }
+    if (hours === 0 && minutes === 0 && seconds ===0){
+      clearInterval(interval);
+      alert("Waktu habis! Jawaban Anda akan dikirimkan");
+      cekJawaban();
+    }
+    timer.innerHTML = `Timer : ${hours}:${minutes}:${seconds}`;
+  }, 1000);
